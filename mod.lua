3f6ff194b4c4d86c61c51080063f12b6659a11b3
Akhtar Muhammad Achsan <64370286+Macrune@users.noreply.github.com>
2024-11-16 02:03:42 +0700

    Change Tool to data.json
    Transfer tools to data.json for easier management. Unfinished scanning of dictionary to automate finding tools and targets

--- mod.lua
+++ mod.lua
@@ -23,17 +23,28 @@ function init()
   -- if you dont return success here your mod will not load
   -- this can be useful if your define fails as you can decide to NOT return "Success" to tell APICO 
   -- that something went wrong and to ignore your mod
+  
   return "Success"
 end
 
 
 function ready()
-
+  api_get_data()
+  api_log("ready", "getting tools")
+  get_all_tools()
   api_log("ready", "loaded mod")
 end
 
+function data(ev, data)
+  if ev == "LOAD" and data ~= nil then
+    tools = data["tools"]
+    axes = data["axes"]
+    pickaxes = data["pickaxes"]
+    spades = data["spades"]
+  end
+end
+
 function click(button, click_type)
-  local original_index
   if button == "LEFT" and click_type == "PRESSED" then
     api_log("clicl", "left click PRESSED")
     original_index = select_tool()
--- modules/autotools.lua
+++ modules/autotools.lua
@@ -1,3 +1,73 @@
+function get_all_tools()
+    local game_dictionary = api_describe_dictionary(false)
+
+    -- Tools tables
+    local dictTools = {}
+    local dictAxes = {}
+    local dictPickaxes = {}
+    local dictSpades = {}
+
+    --Target tables
+    local dictTrees = {}
+    local dictRocks = {}
+    local dictSeeds = {}
+
+    for key, val in pairs(game_dictionary) do
+        -- Checks if item is an axe and then add them to table
+        if string.find(key, "axe") and not string.find(key, "pickaxe") then
+            table.insert(dictAxes, key)
+        end
+
+        -- Checks if item is a pickaxe and then add them to table
+        if string.find(key, "pickaxe") then
+            table.insert(dictPickaxes, key)
+        end
+
+        -- Checks if item is a spade and then add them to table
+        if string.find(key, "spade") then
+            table.insert(dictSpades, key)
+        end 
+
+        --Checks if item has tools describtion for target tables
+        if val["tools"] ~= nil then
+            --Loop to find what tools is usable to target
+            for tool in val["tools"] do
+                --If tool is an axe then its a tree
+                if string.find(tool, "axe") and not string.find(tool, "pickaxe") then
+                    table.insert(dictTrees, key)
+                end
+
+                --If tool is a pickaxe then its a rock
+                if string.find(tool, "pickaxe") then
+                    table.insert(dictTrees, key)
+                end
+
+                --if tool is a spaded then its a seed
+                if string.find(tool, "spade") then
+                    table.insert(dictTrees, key)
+                end
+            end
+        end
+    end
+
+    api_log("get_all_tools", tostring(count))
+    dump(dictAxes)
+    dump(dictPickaxes)
+    dump(dictSpades)
+    dump(dictTrees)
+    dump(dictRocks)
+    dump(dictSeeds)
+end
+
+function dump(o)
+    local str = ""
+    api_log("dump", "Dumping")
+    for _, val in pairs(o) do
+        str = str..val.." "
+
+    api_log("dump", str)
+    end
+end
 
 function check_highlight()
     local highlighted = api_get_highlighted('obj')
@@ -6,8 +76,9 @@ function check_highlight()
 
     if highlighted ~= nil and not isUsingTool then
         local inst = api_get_inst(highlighted)
+        api_log("check_highlight", tostring(inst["oid"]))
         if inst ~= nil then
-            if inst["oid"] == "tree" or inst["oid"] == "bush" then
+            if inst["oid"] == "tree" or inst["oid"] == "shrub" then
                 return "axe"
             elseif inst["oid"] == "rock1" or inst["oid"] == "rock2" then
                 return "pickaxe"
@@ -24,16 +95,8 @@ function check_highlight()
 end
 
 function check_tool_equiped()
-    local tools = {"axe1", "axe2", "axe3", "axe4",
-            "pickaxe1", "pickaxe2", "pickaxe3",
-            "spade1", "spade2", "spade3",
-            "hammer1", "hammer2", "hammer3",
-            "shovel3", "net", "paintbrush", "scrapper",
-            "mag", "pencil", "wateringcan"}
-
     local equipped = api_get_equipped()
     local isTool = false
-
     for _, tool in pairs(tools) do
         if equipped == tool then
             isTool = true
@@ -45,11 +108,7 @@ function check_tool_equiped()
 end
 
 function select_tool()
-    local axes = {"axe1", "axe2", "axe3", "axe4"}
-    local pickaxes = {"pickaxe1", "pickaxe2", "pickaxe3"}
-    local spades = {"spade1", "spade2", "spade3"}
     local tool = check_highlight()
-
     if tool ~= nil then
         local player_id = api_get_player_instance()
         local hotbar = api_get_slots(player_id)
@@ -65,7 +124,7 @@ function select_tool()
         end
 
         if tool_index ~= nil then
-            api_sp(player_id, "hotbar", tool_index)
+            api_sp(player_id, "hotbar", tool_index.index-1)
         end
 
         return cur_index
@@ -78,7 +137,6 @@ end
 function return_to_index(index)
     if index ~= nil then
         local player_id = api_get_player_instance()
-        api_log("return_to_index", "hotbar returned")
         api_sp(player_id, "hotbar", index)
     end 
 end
